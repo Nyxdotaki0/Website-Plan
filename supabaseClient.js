c689091187f24bc30f95fa17683ae08e33897383
Nyxakio <R1109769@students.katyisd.org>
2026-06-07 00:51:56 -0500

    Proxy Supabase through Vercel

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,6 +1,6 @@
 ﻿import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
-const supabaseUrl = "https://nullverse.dev/supabase";
+const supabaseUrl = "https://baaygzddafopihtxyyjq.supabase.co";
 const supabaseAnonKey = 'sb_publishable_O0JPmpCSe7TDoUxotIe4aQ_4EBgEBV8';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
